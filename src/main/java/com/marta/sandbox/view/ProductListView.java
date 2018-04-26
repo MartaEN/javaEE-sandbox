2cbd4c4c7f1370b0d45774371333fcfcc52fd7d0
MartaEN <marta.the.developer@gmail.com>
2018-04-26 17:42:22 +0300

    Interceptor added

--- src/main/java/com/marta/sandbox/view/ProductListView.java
+++ src/main/java/com/marta/sandbox/view/ProductListView.java
@@ -2,6 +2,7 @@ package com.marta.sandbox.view;
 
 import com.marta.sandbox.dao.ProductDAO;
 import com.marta.sandbox.entity.Product;
+import com.marta.sandbox.logger.DebugLogger;
 import org.primefaces.event.SelectEvent;
 
 import javax.enterprise.context.RequestScoped;
@@ -9,6 +10,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.interceptor.Interceptors;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,26 +26,32 @@ public class ProductListView implements Serializable {
 
     public ProductListView () {}
 
+    @Interceptors(DebugLogger.class)
     public List<Product> getProducts() {
         return new ArrayList<>(dao.getListProduct());
     }
 
+    @Interceptors(DebugLogger.class)
     public void addProduct() {
         dao.persist(new Product());
     }
 
+    @Interceptors(DebugLogger.class)
     public void remove(Product product) {
         dao.removeProduct(product.getId());
     }
 
+    @Interceptors(DebugLogger.class)
     public Product getSelectedProduct() {
         return selectedProduct;
     }
 
+    @Interceptors(DebugLogger.class)
     public void setSelectedProduct(Product selectedProduct) {
         this.selectedProduct = selectedProduct;
     }
 
+    @Interceptors(DebugLogger.class)
     public void onRowSelect(SelectEvent event) {
         FacesMessage msg = new FacesMessage("Item Selected", ((Product)event.getObject()).getId());
         FacesContext.getCurrentInstance().addMessage(null, msg);
